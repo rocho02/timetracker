ad972cb79310176ef335f25759d5105119525222
Roland Schneider <rschneider@ucs.at>
2021-07-15 11:36:33 +0200

    fix awk getting only first word

--- timetracker.sh
+++ timetracker.sh
@@ -7,7 +7,7 @@ function track(){
         FILEPATH=$(/bin/readlink -f /home/"$USER"/timetracker)
         FILE="${FILEPATH}/done.${DATE}.log"
         # echo "wirting to $FILE"  
-        /usr/bin/zenity --title="TimeTracker" --width=400 --height=300 --timeout=30  --entry --text="What are you doing?" | awk -v time="$TIME" '{ logline=time" "$1; print logline }'  >> $FILE
+        /usr/bin/zenity --title="TimeTracker" --width=400 --height=300 --timeout=30  --entry --text="What are you doing?" | awk -v time="$TIME" '{ logline=time" "$0; print logline }'  >> $FILE
 }
 # usally it should be ":0" , but for me somehow it is :0
   export DISPLAY=:1
