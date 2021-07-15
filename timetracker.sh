837678dd8f2c718c5e8ce7cb658878287b8a4b56
Roland Schneider <rschneider@ucs.at>
2021-07-15 09:02:41 +0200

    improve timetracker

--- timetracker.sh
+++ timetracker.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-USER="rocho"
+USER=$(whoami)
 function track(){
         mkdir -p /home/"$USER"/timetracker
         DATE=$(date '+%Y-%m-%d')
         TIME=$(date '+%Y-%m-%d %H:%M:%S')
         FILEPATH=$(/bin/readlink -f /home/"$USER"/timetracker)
         FILE="${FILEPATH}/done.${DATE}.log"
-        echo "wirting to $FILE"  
-        /usr/bin/zenity --entry --text="What are you doing?" | awk -v time="$TIME" '{ logline=time" "$1; print logline }'  >> $FILE
+        # echo "wirting to $FILE"  
+        /usr/bin/zenity --title="TimeTracker" --width=400 --height=300 --timeout=30  --entry --text="What are you doing?" | awk -v time="$TIME" '{ logline=time" "$1; print logline }'  >> $FILE
 }
+# usally it should be ":0" , but for me somehow it is :0
   export DISPLAY=:1
 track
 
